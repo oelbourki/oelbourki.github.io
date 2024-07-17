b3abaaada0d50eabd422ae27330fef4342206703
oelbourki <elbourki.1999@gmail.com>
2024-07-17 22:42:04 +0100

    changes

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -41,13 +41,14 @@ const IconLogo = () => (
         />
       </g>
     </g>
-    <text textAnchor="middle" alignmentBaseline="middle" fill="currentColor" fontSize="55px" fontFamily="Consolas, serif" x="50%" y="50%">
-
-      O
-    </text>
   </svg>
 );
+{
+  /* <text textAnchor="middle" alignmentBaseline="middle" fill="currentColor" fontSize="55px" fontFamily="Consolas, serif" x="50%" y="50%">
 
+O
+</text> */
+}
 export default IconLogo;
 // import React from "react";
 
