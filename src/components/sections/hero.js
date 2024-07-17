638d6e98711e118db67e082da3950039e0442be6
oelbourki <elbourki.1999@gmail.com>
2024-07-17 17:30:51 +0100

    change metadata

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,7 +60,7 @@ const StyledHeroSection = styled.section`
 //   }, []);
 
 //   const one = <h1>Hi, my name is</h1>;
-//   const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+//   const two = <h2 className="big-heading">Otmane El Bourki.</h2>;
 //   const three = <h3 className="big-heading">I build things for the web.</h3>;
 //   const four = (
 //     <>
