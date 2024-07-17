d70873f42e257e155c525b3982b4ab43f7ed134c
oelbourki <elbourki.1999@gmail.com>
2024-07-17 17:30:08 +0100

    change metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,10 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Otmane El Bourki',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Otmane El Bourki is an AI engineer who specializes in building AI apps.',
+    siteUrl: 'https://oelbourki.site', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
